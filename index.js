6c28a07776929c3acf55a413c50a827f7910242e
stelllzx <01stellaochuba@gmaol.com>
2023-08-26 11:33:02 +0100

    I FOLLOWED A TUTORIAL

---
+++ index.js
@@ -0,0 +1,12 @@
+//This is my first JavaScript code!
+console.log('Hello World');
+let name = 'stella';
+console.log(name);
+let lastName = 'Ochuba';
+console.log(lastName); // string literal
+let intrestRate = 0.3;
+intrestRate = 1;
+console.log(intrestRate);
+let age = 30; // Number Literal
+let isApproved = true; // Boolean Literal
+let className = null;
